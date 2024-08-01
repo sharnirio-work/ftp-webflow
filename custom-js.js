d0b59f9bf77048674b5de123d0a81c2442809105
Vyacheslav Gorisnyack <sharnirio@gmail.com>
2024-08-01 15:14:57 +0300

    some fix

--- custom-js.js
+++ custom-js.js
@@ -1,1 +1,19 @@
-console.log('123');
+// const counterUp = window.counterUp.default
+
+// const callback = entries => {
+// 	entries.forEach( entry => {
+// 		const el = entry.target
+// 		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+// 			counterUp( el, {
+// 				duration: 2000,
+// 				delay: 1000,
+// 			} )
+// 			el.classList.add( 'is-visible' )
+// 		}
+// 	} )
+// }
+
+// const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+// const el = document.querySelector( '.counter' )
+// IO.observe( el )
